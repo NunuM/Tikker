59e2ba7aa60ec49c628864af60fe057104d9daf6
Nuno Gonalves <1140358@isep.ipp.pt>
2015-11-08 20:10:41 +0000

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,16 +1,6 @@
 angular.module('tikker.controllers', ['tikker.factory', 'ngResource', 'youtube-embed'])
 
         .controller('AppCtrl', function ($scope) {
-
-            // With the new view caching in Ionic, Controllers are only called
-            // when they are recreated or on app start, instead of every page change.
-            // To listen for when this page is active (for example, to refresh data),
-            // listen for the $ionicView.enter event:
-            //$scope.$on('$ionicView.enter', function(e) {
-            //});
-
-
-
         })
 
         .controller('videosCtrl', function ($scope, reddit) {
